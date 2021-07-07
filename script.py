163e24df8bcbb77dad685665bc31dadf1f648f73
Irenitemi Agbejule <irenitemi81@gmail.com>
2021-07-08 00:45:58 +0100

    Accepts cmd line args

--- script.py
+++ script.py
@@ -1,11 +1,28 @@
-
+#!/usr/bin/env python
 import subprocess
 import os
-batcmd="system_profiler SPUSBDataType"
-connected_devices = subprocess.check_output(batcmd, shell=True)
+import sys
+print('here')
+print(sys.argv)
+if len(sys.argv) == 3:
+    driveName = sys.argv[2]
+    cardName = sys.argv[1]
+    batcmd="system_profiler SPUSBDataType"
+    connected_devices = subprocess.check_output(batcmd, shell=True)
+    isCameraConn = "/Volumes/{}".format(cardName) in str(connected_devices)
+    isDriveConn = "/Volumes/{}".format(driveName) in str(connected_devices)
+    print(isCameraConn,isDriveConn)
+    if isCameraConn and isDriveConn:
+        setOnCamera = set(os.listdir("/Volumes/{}/DCIM/102MSDCF".format(cardName)))
+        setOnComp = set(os.listdir("/Volumes/{}/Pictures"))
+        for i in setOnCamera.difference(setOnComp):
+            os.system('cp /Volumes/{}/DCIM/102MSDCF/{} /Volumes/{}/Pictures/{}'.format(cardName,i,driveName,i))
+    elif not isCameraConn and isDriveConn:
+        print("Error:The camera isnt connected")
 
-if "/Volumes/CAMERA" in str(connected_devices):
-    setOnCamera = set(os.listdir("/Volumes/CAMERA/DCIM/102MSDCF"))
-    setOnComp = set(os.listdir("/Users/temi/Pictures"))
-    for i in setOnCamera.difference(setOnComp):
-        os.system('cp /Volumes/CAMERA/DCIM/102MSDCF/{} /Users/temi/Pictures/{}'.format(i,i))
+    elif isCameraConn and not isDriveConn:
+        print("Error: The drive isnt connected")
+    else:
+        print('Neither device is connected')
+else:
+    print('Error: [Name of sd card] [Name of drive]')
