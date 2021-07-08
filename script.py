a38431f9cfc479142a96e99865c5a3ae4beae775
Irenitemi Agbejule <irenitemi81@gmail.com>
2021-07-08 12:13:28 +0100

    Checks if Pictures dir exists in drive

--- script.py
+++ script.py
@@ -2,8 +2,6 @@
 import subprocess
 import os
 import sys
-print('here')
-print(sys.argv)
 if len(sys.argv) == 3:
     driveName = sys.argv[2]
     cardName = sys.argv[1]
@@ -11,18 +9,20 @@ if len(sys.argv) == 3:
     connected_devices = subprocess.check_output(batcmd, shell=True)
     isCameraConn = "/Volumes/{}".format(cardName) in str(connected_devices)
     isDriveConn = "/Volumes/{}".format(driveName) in str(connected_devices)
-    print(isCameraConn,isDriveConn)
     if isCameraConn and isDriveConn:
+        if not os.path.exists("/Volumes/{}/Pictures".format(driveName)):
+            os.makedirs("/Volumes/{}/Pictures".format(driveName))
         setOnCamera = set(os.listdir("/Volumes/{}/DCIM/102MSDCF".format(cardName)))
-        setOnComp = set(os.listdir("/Volumes/{}/Pictures"))
+        setOnComp = set(os.listdir("/Volumes/{}/Pictures".format(driveName)))
         for i in setOnCamera.difference(setOnComp):
             os.system('cp /Volumes/{}/DCIM/102MSDCF/{} /Volumes/{}/Pictures/{}'.format(cardName,i,driveName,i))
+            print(i)
     elif not isCameraConn and isDriveConn:
-        print("Error:The camera isnt connected")
+        print("Error: The camera isn't connected")
 
     elif isCameraConn and not isDriveConn:
-        print("Error: The drive isnt connected")
+        print("Error: The drive isn't connected")
     else:
-        print('Neither device is connected')
+        print('Error: Neither device is connected')
 else:
     print('Error: [Name of sd card] [Name of drive]')
